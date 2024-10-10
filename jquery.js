5a9350f549061b036bb4e3e39e843181d6fd6015
FabriC4 <160611967+FabriC4@users.noreply.github.com>
2024-10-10 16:32:05 -0300

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,102 @@
+
+/*Actividad 2.*/
+/*let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+    let p_ = $("#Primer_Parrafo")
+    p_.click(obtener_parrafo)
+}
+
+
+function obtener_parrafo() {
+    alert("click en parrafo 1");
+}*/
+
+/*Actividad 3.*/
+/*let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+  let x = $("#fila1");
+  x.click(presionFila1)
+  x = $("#fila2");
+  x.click(presionFila2)
+}
+
+function presionFila1() {
+  let x = $("#fila1");
+  x.css("color", "#ff0000");
+  x.css("background-color", "#ffff00");
+  x.css("font-family", "Courier");
+}
+
+function presionFila2() {
+  let x = $("#fila2");
+  x.css("color", "#ffff00");
+  x.css("background-color", "#ff0000");
+  x.css("font-family", "Arial");
+}*/
+
+/*Actividad 4*/
+/*let x=$(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos()
+{
+  let x=$("#list li");
+  x.click(ocultarFila);
+}
+
+function ocultarFila()
+{
+  let x=$(this);
+  x.hide();
+}*/
+
+/*Actividad 5*/
+/*let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+  let x = $("#tabla1 tr");
+  x.click(presionTabla)
+}
+
+function presionTabla() {
+  let x = $("#tabla1 tr");
+  x.css("color", "#ff0000");
+  x.css("background-color", "#ffff00");
+  x.css("font-family", "Courier");
+}
+*/
+
+/*Actividad 6*/
+/*let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+  let x1 = $("#boton1");
+  x1.click(cambiarTamaño1);
+  let x2 = $("#boton2");
+  x2.click(cambiarTamaño2);
+  let x3 = $("#boton3");
+  x3.click(cambiarTamaño3);
+}
+
+function cambiarTamaño1() {
+  let x = $(".descripcion1");
+  x.css("font-size", "20px");
+}
+
+function cambiarTamaño2() {
+  let x = $(".descripcion2");
+  x.css("font-size", "22px");
+}
+
+function cambiarTamaño3() {
+  let x = $(".descripcion3");
+  x.css("font-size", "24px");
+}*/
+
+/*Actividad 7*/
